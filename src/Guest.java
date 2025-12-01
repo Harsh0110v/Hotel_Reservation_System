614e754d29001d921630fee4d5a4b8d305614444
Harsh Varma <harshvarma0110@gmail.com>
2025-12-01 22:48:23 +0100

    basic errors

--- src/Guest.java
+++ src/Guest.java
@@ -1,23 +1,29 @@
 import java.io.Serializable;
 
-public class Guest implements Serializable {
+public class Guest implements Serializable{
     private String memberId;
     private String name;
     private String phone;
     private String email;
     private int age;
 
-    public Guest(String memberId, String name, String phone, String email, int age) {
+    public Guest(String memberId, String name, String phone, String email, int age){
         this.memberId = memberId;
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.age = age;
+        this.name= name;
+        this.phone= phone;
+        this.email= email;
+        this.age= age;
     }
 
-    public String getMemberId() { return memberId; }
-    public String getName() { return name; }
-    public String getPhone() { return phone; }
-    public String getEmail() { return email; }
-    public int getAge() { return age; }
+    public String getMemberId(){
+        return memberId; 
+                                }
+    public String getName(){
+        return name; }
+    public String getPhone(){
+        return phone; }
+    public String getEmail(){
+        return email; }
+    public int getAge(){
+        return age; }
 }
